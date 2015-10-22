ec2a4bf9ae59673d33e0f705aba6b3ba2e3bf663
Arthur Rousseau <arthur.rousseau2@gmail.com>
2015-10-22 09:14:40 +0200

    Post merge refactoring

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,4 +1,15 @@
 #!/usr/bin/env sh
 
-git clone git@github.com:iTranslate/core.git
-browserify /core/src/main.js -o ./congolexicomatisation.js
+# Retrieving iTranslate core
+rm -rf ./tmp/
+git clone https://github.com/iTranslate/core ./tmp/core/
+rm -rf ./tmp/core/src/dictionnaries
+rm -f ./tmp/core/src/dictionnaries.js
+cp -R ./src/dictionnaries ./tmp/core/src/
+cp ./src/dictionnaries.js ./tmp/core/src/
+
+# Generating extension
+npm install -g browserify
+browserify ./tmp/core/src/main.js -o ./congolexicomatisation.js
+npm build
+rm -rf ./tmp/
