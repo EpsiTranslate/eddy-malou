2a60323984f65d6a62816033be7591824ad350c9
Malo Blanchard <git@maloblanchard.com>
2015-11-03 23:23:06 +0100

    fix(scripts): update name of entry point of the extension

--- scripts/build.sh
+++ scripts/build.sh
@@ -10,5 +10,5 @@ cp ./src/dictionnaries.js ./tmp/core/src/
 
 # Generating extension
 npm install -g browserify
-browserify ./tmp/core/src/main.js -o ./congolexicomatisation.js
+browserify ./tmp/core/src/main.js -o ./app.js
 rm -rf ./tmp/
